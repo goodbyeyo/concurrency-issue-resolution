6fadc1705eefed4a74c064665cf6b8ef55ea4768
Sangwook Hwang <yoonseol1983@gmail.com>
2022-09-17 16:47:26 +0900

    nameLock release_lock funtion name error 수정

--- src/main/java/study/stock/repository/LockRepository.java
+++ src/main/java/study/stock/repository/LockRepository.java
@@ -6,14 +6,15 @@ import study.stock.domain.Stock;
 
 /**
  * 실무에서 NamedLock 을 사용할때는 DataSource 를 분리해서 사용해야한다
+ * - nameLock : connection 2 개 사용 ( lock 획득 1개, transaction 로직 1개 )
  * 즉 별도의 JDBC 를 통해 Connection 을 하는것을 추천
- * 같은 DataSource를 사용하면 ConnectionPool이 부족해져서 다른 서비스에 영향을 미칠수 있다
+ * 같은 dataSource 사용하면 connection pool 부족으로 다른 서비스에 영향을 미칠수 있다
  */
 public interface LockRepository extends JpaRepository<Stock, Long> {
     @Query(value = "select get_lock(:key, 3000)", nativeQuery = true)
     void getLock(String key);
 
-    @Query(value = "select get_release(:key)", nativeQuery = true)
+    @Query(value = "select release_lock(:key)", nativeQuery = true)
     void releaseLock(String key);
 
 }
--- src/main/java/study/stock/service/StockService.java
+++ src/main/java/study/stock/service/StockService.java
@@ -13,8 +13,19 @@ public class StockService {
 
     private final StockRepository stockRepository;
 
+    /**
+     * NamedLock 사용시 synchronized 제거 필요
+     * Transactional 어노테이션이 있어야 getLock 과 releaseLock() 이 사용하는 커넥션이 달라진다
+     * Propagation.REQUIRES_NEW 사용 이유
+     * -> 부모의 트랜잭션과 동일한 범위로 묶인다면 synchronized 와 같은 문제 발생
+     * -> 즉 Database 에 commit 되기전에 락이 풀리는 현상 발생
+     * -> 따라서 별도의 트랜잭션으로 분리하여 Database 에 정상적으로 commit 된 이후에 락을 해제
+     * -> propagation 을 변경하면 새로운 트랜잭션을 시작하기때문에 hikari.maximum-pool-size 의 개수 넉넉히 늘려주는것이 좋다
+     * -> 정리하면 lock 을 해제하기전에 Database 에 Commit 이 되도록 하는것
+     */
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public synchronized void decrease(Long id, Long quantity) {
+    public void decrease(Long id, Long quantity) {
+//    public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
         stockRepository.saveAndFlush(stock);
